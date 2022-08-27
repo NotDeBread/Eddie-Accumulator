b2cf8972ca80959083278d1610e03959c36b9de2
DeBread <104747002+NotDeBread@users.noreply.github.com>
2022-08-27 02:51:49 -0500

    BETA v0.02
    -Settings
       ⮡ Delete Data
       ⮡ Autosaves
    -New Upgrade
       ⮡ Platinum Fingers
          ⮡ Multiplies CPC by 2
    -Save Indicator

--- script.js
+++ script.js
@@ -20,26 +20,39 @@ const config = JSON.parse(localStorage.getItem('save')) ?? {
     boneprice: 100,
     ballprice: 500,
 
+    
+    
+    totalupgrade: 0,
+    upgrade_goldenfingers_bought: false,
+    upgrade_platinumfingers_bought: false,
 
-    upgrade_goldenfingers_bought: false
+    setting1var: true,
+
+    autosave: true,
 }
 
-
+var settingsopen = false
 
 var cps = 0
 
+var noti_countdown = 0
+
+var upgrades = 1
+var cursormultiplier = 1
+
 setInterval(clickspersecond, 10)
 setInterval(update, 1)
-setInterval(upgrade_goldenfingers_update, 1)
+setInterval(upgrade_update, 1)
 setInterval(autosave, 60000)
 
 function eddieclick() {
     config.countraw += config.cpc
+
     document.getElementById('count').innerHTML = config.count;
 
     if (config.countraw >= config.cursorprice) {
         document.getElementById('cursorimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('cursorprice').style.setProperty('color', 'green')
+        document.getElementById('cursorprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('cursorimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('cursorprice').style.setProperty('color', 'red')
@@ -47,16 +60,79 @@ function eddieclick() {
 
     if (config.count >= config.boneprice) {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('boneprice').style.setProperty('color', 'green')
+        document.getElementById('boneprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('boneprice').style.setProperty('color', 'red')
     }
 }
 
-function autosave(){
+//Settings
+
+function settings() {
+
+//Open
+
+    if(!settingsopen) {
+        document.getElementById('settings_img').src = "images/icons/back.png"
+        document.getElementById('settings').style.setProperty('width', '250px')
+        document.getElementById('settings').style.setProperty('height', '100px')
+        document.getElementById('settings').style.setProperty('background-color', 'var(--darkerbg)')
+        document.getElementById('settings_content').style.setProperty('opacity', '1')
+        document.getElementById('settings_header').style.setProperty('color', 'rgba(255, 255, 255, 255)')
+    }
+
+//Close
+
+    if(settingsopen) {
+        document.getElementById('settings_img').src = "images/icons/settings.png"
+        document.getElementById('settings').style.setProperty('width', '40px')
+        document.getElementById('settings').style.setProperty('height', '40px')
+        document.getElementById('settings').style.setProperty('background-color', 'transparent')
+        document.getElementById('settings_content').style.setProperty('opacity', '0')
+        document.getElementById('settings_header').style.setProperty('color', 'rgba(255, 255, 255, 0)')
+    }
+    settingsopen = !settingsopen
+}
+
+function setting1() {
+    if(!config.setting1var) {
+        document.getElementById('setting1-img').src = "images/icons/checkbox2.png"
+        config.autosave = true
+    } 
+    
+    if(config.setting1var) {
+        document.getElementById('setting1-img').src = "images/icons/checkbox1.png"
+        config.autosave = false
+    }
+
+    config.setting1var = !config.setting1var
+}
+
+function autosave() {
+    if(config.autosave === true) {
+    localStorage.setItem('save', JSON.stringify(config))
+    console.log('AutoSaved', config.count, 'Eddie Orbs!')
+    savenoti()
+    }
+}
+
+function save() {
     localStorage.setItem('save', JSON.stringify(config))
     console.log('Saved', config.count, 'Eddie Orbs!')
+    savenoti()
+}
+
+function savenoti() {
+    document.getElementById('savenoti').style.setProperty('opacity','1')
+    setTimeout(() => {
+        document.getElementById('savenoti').style.setProperty('opacity','0')
+    }, 3000);
+}
+
+function deletesave(){
+    localStorage.removeItem('save', JSON.stringify(config))
+    window.location.reload();
 }
 
 //----------BUILDINGS----------//
@@ -78,7 +154,7 @@ function buycursor() {
 
         if (config.countraw >= config.cursorprice) {
             document.getElementById('cursorimg').style.setProperty('filter', 'brightness(100%)')
-            document.getElementById('cursorprice').style.setProperty('color', 'green')
+            document.getElementById('cursorprice').style.setProperty('color', 'lime')
         } else {
             document.getElementById('cursorimg').style.setProperty('filter', 'brightness(75%)')
             document.getElementById('cursorprice').style.setProperty('color', 'red')
@@ -103,7 +179,7 @@ function buybone() {
 
         if (config.count >= config.boneprice) {
             document.getElementById('boneimg').style.setProperty('filter', 'brightness(100%)')
-            document.getElementById('boneprice').style.setProperty('color', 'green')
+            document.getElementById('boneprice').style.setProperty('color', 'lime')
         } else {
             document.getElementById('boneimg').style.setProperty('filter', 'brightness(75%)')
             document.getElementById('boneprice').style.setProperty('color', 'red')
@@ -126,7 +202,7 @@ function buyball() {
 
         if (config.count >= config.ballprice) {
             document.getElementById('ballimg').style.setProperty('filter', 'brightness(100%)')
-            document.getElementById('ballprice').style.setProperty('color', 'green')
+            document.getElementById('ballprice').style.setProperty('color', 'lime')
         } else {
             document.getElementById('ballimg').style.setProperty('filter', 'brightness(75%)')
             document.getElementById('ballprice').style.setProperty('color', 'red')
@@ -146,7 +222,7 @@ function clickspersecond() {
 
     if (config.countraw >= config.cursorprice) {
         document.getElementById('cursorimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('cursorprice').style.setProperty('color', 'green')
+        document.getElementById('cursorprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('cursorimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('cursorprice').style.setProperty('color', 'red')
@@ -154,7 +230,7 @@ function clickspersecond() {
 
     if (config.count >= config.boneprice) {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('boneprice').style.setProperty('color', 'green')
+        document.getElementById('boneprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('boneprice').style.setProperty('color', 'red')
@@ -162,7 +238,7 @@ function clickspersecond() {
 
         if (config.count >= config.boneprice) {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('boneprice').style.setProperty('color', 'green')
+        document.getElementById('boneprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('boneimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('boneprice').style.setProperty('color', 'red')
@@ -170,18 +246,26 @@ function clickspersecond() {
 
     if (config.count >= config.ballprice) {
         document.getElementById('ballimg').style.setProperty('filter', 'brightness(100%)')
-        document.getElementById('ballprice').style.setProperty('color', 'green')
+        document.getElementById('ballprice').style.setProperty('color', 'lime')
     } else {
         document.getElementById('ballimg').style.setProperty('filter', 'brightness(75%)')
         document.getElementById('ballprice').style.setProperty('color', 'red')
     }
 
     if(config.count >= 1000){
-        document.getElementById('upgrade_goldenfingers_price').style.setProperty('color', 'green')
+        document.getElementById('upgrade_goldenfingers_price').style.setProperty('color', 'lime')
         document.getElementById('upgrade_goldenfingers_img').style.setProperty('filter', 'brightness(100%)')
     } else {
         document.getElementById('upgrade_goldenfingers_price').style.setProperty('color', 'red')
         document.getElementById('upgrade_goldenfingers_img').style.setProperty('filter', 'brightness(75%)')
+    }
+
+    if(config.count >= 10000){
+        document.getElementById('upgrade_platinumfingers_price').style.setProperty('color', 'lime')
+        document.getElementById('upgrade_platinumfingers_img').style.setProperty('filter', 'brightness(100%)')
+    } else {
+        document.getElementById('upgrade_platinumfingers_price').style.setProperty('color', 'red')
+        document.getElementById('upgrade_platinumfingers_img').style.setProperty('filter', 'brightness(75%)')
     }
 }
 
@@ -224,19 +308,43 @@ function upgrade_goldenfingers_hover() {
     document.getElementById('info_label').innerHTML = "";
 }
 
+function upgrade_platinumfingers_hover() {
+    document.getElementById('info_title').innerHTML = "Platinum Fingers";
+    document.getElementById('info_cps').innerHTML = "Changes the CPC based on how many cursors are owned.";
+    document.getElementById('info_owned').style.setProperty('color', 'transparent');
+    document.getElementById('owned_label').style.setProperty('opacity', '0');
+    document.getElementById('info_label').innerHTML = "";
+}
+
 function upgrade_goldenfingers() {
     if(config.upgrade_goldenfingers_bought === false && config.count >= 1000) {
         document.getElementById('upgrade_goldenfingers').style.setProperty('display', 'none')
         config.upgrade_goldenfingers_bought = true
         config.countraw -= 1000
+        cursormultiplier += 1
     }
 }
 
-function upgrade_goldenfingers_update() {
-    if(config.upgrade_goldenfingers_bought === true && config.cursortotal >= 1) {
-        config.cpc = config.cursortotal;
-
+function upgrade_platinumfingers() {
+    if(config.upgrade_platinumfingers_bought === false && config.count >= 10000) {
+        document.getElementById('upgrade_platinumfingers').style.setProperty('display', 'none')
+        config.upgrade_goldenfingers_bought = true
+        config.countraw -= 10000
+        cursormultiplier *= 2
     }
+}
+
+function upgrade_update() {
+    if(config.upgrade_platinumfingers_bought === true && config.cursortotal >=1) {
+        config.cpc = config.cursortotal * cursormultiplier;
+        document.getElementById('upgrade_platinumfingers').style.setProperty('display', 'none')
+    }
+        
+    if(config.upgrade_goldenfingers_bought === true && config.cursortotal >= 1) {
+            config.cpc = config.cursortotal * cursormultiplier
+            document.getElementById('upgrade_goldenfingers').style.setProperty('display', 'none')
+    }
+    
 }
 
 //----------EVENTS----------//
@@ -246,8 +354,12 @@ function update() {
     cps = (config.cursorcps + config.bonecps + config.ballcps)
 
     config.count = Math.round(config.countraw)
+    
     if(config.cursortotal >= 10){
         document.getElementById('upgrade_goldenfingers').hidden = false;
+    }
+    if(config.cursortotal >= 25){
+        document.getElementById('upgrade_platinumfingers').hidden = false;
     }
 }
 
